2b810760317f81055e7dfc320424593f16f4c797
MOZGIII <mike-n@narod.ru>
2019-11-12 13:41:13 +0300

    Canvas with images

--- src/index.ts
+++ src/index.ts
@@ -10,13 +10,51 @@ const tag = "overlay-image-input";
 
 @customElement(tag)
 class OverlayImageInput extends LitElement {
-  @property()
-  name = "world";
+  @property({ attribute: false })
+  backgroundImage?: CanvasImageSource = undefined;
+
+  @property({ type: Number, attribute: "bgx" })
+  backgroundX = 0;
+
+  @property({ type: Number, attribute: "bgy" })
+  backgroundY = 0;
+
+  @property({ attribute: false })
+  image?: CanvasImageSource = undefined;
+
+  @property({ type: Number })
+  x = 0;
+
+  @property({ type: Number })
+  y = 0;
 
   render(): TemplateResult {
     return html`
-      <p>hello ${this.name}</p>
+      <canvas id="canvas"></canvas>
     `;
+  }
+
+  firstUpdated() {
+    const root = this.shadowRoot;
+    if (!root) {
+      return;
+    }
+    const canvas = root.getElementById("canvas") as HTMLCanvasElement | null;
+    if (!canvas) {
+      return;
+    }
+
+    const ctx = canvas.getContext("2d");
+    if (!ctx) {
+      return;
+    }
+
+    if (this.backgroundImage) {
+      ctx.drawImage(this.backgroundImage, this.backgroundX, this.backgroundY);
+    }
+    if (this.image) {
+      ctx.drawImage(this.image, this.x, this.y);
+    }
   }
 }
 
